bde402eeba04fe6b55c3b565c6fc8a5386fd124c
charlesliew <clarles.liew@claritytech.sg>
2024-03-26 16:44:59 +0800

    Setup project library, create temp splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
+	id("com.google.dagger.hilt.android")
+	id("kotlin-kapt")
 }
 
 android {
@@ -15,6 +17,10 @@ android {
 		versionName = "1.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+		vectorDrawables {
+			useSupportLibrary = true
+		}
 	}
 
 	buildTypes {
@@ -30,6 +36,12 @@ android {
 	kotlinOptions {
 		jvmTarget = "1.8"
 	}
+	buildFeatures {
+		compose = true
+	}
+	composeOptions {
+		kotlinCompilerExtensionVersion = "1.5.11"
+	}
 }
 
 dependencies {
@@ -40,4 +52,35 @@ dependencies {
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+	// Compose
+	implementation("androidx.compose.material3:material3-android:1.2.1")
+	implementation("androidx.activity:activity-compose:1.8.2")
+	implementation("androidx.navigation:navigation-compose:2.7.7")
+	implementation("androidx.compose.ui:ui")
+	implementation("androidx.compose.ui:ui-graphics")
+	implementation("androidx.compose.ui:ui-tooling-preview")
+
+	// Timber
+	implementation("com.jakewharton.timber:timber:5.0.1")
+
+	// Coroutines
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+	// Coroutine Lifecycle Scopes
+	val lifecycleVersion = "2.7.0"
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+
+	// Dagger Hilt
+	// Not support KSP
+	val daggerVersion = "2.44"
+	implementation("com.google.dagger:hilt-android:$daggerVersion")
+	kapt("com.google.dagger:hilt-android-compiler:$daggerVersion")
+}
+
+// Allow references to generated code
+kapt {
+	correctErrorTypes = true
 }
