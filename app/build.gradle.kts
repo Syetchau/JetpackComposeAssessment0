c17e30f617ca7012556a5bf69977da757ffefbe7
charlesliew <clarles.liew@claritytech.sg>
2024-03-27 12:25:34 +0800

    adjust library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
 	// Coroutines
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+	// Test
 	androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
