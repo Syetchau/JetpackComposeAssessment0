9737d1061e299e0132be86b616f6eed8203f8f84
charlesliew <clarles.liew@claritytech.sg>
2024-03-27 19:46:01 +0800

    add listActivity

---
+++ app/src/main/java/io/rapidz/training/views/ListActivity.kt
@@ -0,0 +1,15 @@
+package io.rapidz.training.views
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ListActivity:  ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
