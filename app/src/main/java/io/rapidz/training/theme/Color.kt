c75d089e7079ff0155bd3a82c2f8ab9cc56acdb1
charlesliew <clarles.liew@claritytech.sg>
2024-03-27 18:19:54 +0800

    add icon, string, colorCode, size

--- app/src/main/java/io/rapidz/training/theme/Color.kt
+++ app/src/main/java/io/rapidz/training/theme/Color.kt
@@ -4,4 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 val Color_Light_Blue = Color(0xFFBAC7FF)
 val Color_Light_Grey = Color(0xFFAAAAAA)
+
 val Color_Splash = Color(0xFF026C89)
+
+val Color_Edit_Text_Bg = Color(0xFFBEBEBE)
+val Color_Edit_Text_Focus_Bg = Color(0xFF2C2C2C)
+val Color_Hint_Text = Color(0xFF4B4B4B)
+val Color_Login_Btn = Color(0xFFFC6815)
+val Color_Login_Btn_Disable = Color(0xFFC75500)
--- app/src/main/java/io/rapidz/training/theme/Size.kt
+++ app/src/main/java/io/rapidz/training/theme/Size.kt
@@ -4,8 +4,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 val APP_LOGO_SIZE = 150.dp
+val HEIGHT_50 = 50.dp
 
+val SPACING_8 = 8.dp
+val SPACING_20 = 20.dp
+val SPACING_36 = 36.dp
 val SPACING_50 = 50.dp
 val SPACING_80 = 80.dp
 
+val FONT_SIZE_16 = 16.sp
 val FONT_SIZE_30 = 30.sp
