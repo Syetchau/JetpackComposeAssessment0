0dd09c6c422fb9218ff5d653c1c2460b34602169
charlesliew <clarles.liew@claritytech.sg>
2024-03-28 16:14:14 +0800

    add color, size, string

--- app/src/main/java/io/rapidz/training/theme/Size.kt
+++ app/src/main/java/io/rapidz/training/theme/Size.kt
@@ -6,14 +6,20 @@ import androidx.compose.ui.unit.sp
 val APP_LOGO_SIZE = 150.dp
 val HEIGHT_50 = 50.dp
 
+val SPACING_4 = 4.dp
 val SPACING_8 = 8.dp
+val SPACING_16 = 16.dp
 val SPACING_20 = 20.dp
+val SPACING_32 = 32.dp
 val SPACING_36 = 36.dp
 val SPACING_50 = 50.dp
 val SPACING_80 = 80.dp
 
+val FONT_SIZE_12 = 12.sp
+val FONT_SIZE_14 = 14.sp
 val FONT_SIZE_16 = 16.sp
+val FONT_SIZE_20 = 20.sp
 val FONT_SIZE_30 = 30.sp
 
-val FONT_WEIGHT_600 = 600
-val FONT_WEIGHT_700 = 700
+const val FONT_WEIGHT_600 = 600
+const val FONT_WEIGHT_700 = 700
