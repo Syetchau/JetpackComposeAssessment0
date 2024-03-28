4c67c657225ab30a9aa8e8ae7778334ce6b55b54
charlesliew <clarles.liew@claritytech.sg>
2024-03-28 17:19:37 +0800

    modify custom drawable

--- app/src/main/java/io/rapidz/training/utils/DrawableUtils.kt
+++ app/src/main/java/io/rapidz/training/utils/DrawableUtils.kt
@@ -2,16 +2,29 @@ package io.rapidz.training.utils
 
 import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 
 object DrawableUtils {
 
     @Composable
-    fun Drawable(modifier: Modifier, drawable: Drawable, contentDescription: String?= null) {
-        Box(modifier) {
+    fun Drawable(
+        modifier: Modifier,
+        drawable: Drawable,
+        contentDescription: String?= null,
+        isClickable: Boolean = false,
+        onClickEvent: (() -> Unit?)? =null
+    ) {
+        Box(
+            modifier = modifier.clickable(enabled = isClickable) {
+                onClickEvent?.invoke()
+            },
+            contentAlignment = Alignment.Center
+        ) {
             Image(
                 painter = rememberDrawablePainter(drawable = drawable),
                 contentDescription = contentDescription,
