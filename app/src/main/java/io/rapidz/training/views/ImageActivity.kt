ef4e77a354d3bc6b84272a5caf45f706b579e375
charlesliew <clarles.liew@claritytech.sg>
2024-03-28 16:13:55 +0800

    create imageScreen

---
+++ app/src/main/java/io/rapidz/training/views/ImageActivity.kt
@@ -0,0 +1,17 @@
+package io.rapidz.training.views
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ImageActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
